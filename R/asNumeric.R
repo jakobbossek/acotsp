8996a40f118fd8aba0c2b3bf87b003c5061b2a76
Jakob Bossek <info@jakobbossek.de>
2015-01-22 19:08:03 +0100

    add as.numeric generic for AntsResult object

---
+++ R/asNumeric.R
@@ -0,0 +1,15 @@
+#' Extracts the best tour found from an AntResult object.
+#'
+#' @param x [\code{AntResult}]\cr
+#'   Result object of a run of the \code{\link{aco}} function.
+#' @param close [\code{logical(1)}]\cr
+#'   Close the tour, i. e., append the last city to the end?
+#' @param ... [any]\cr
+#'    Currenly not used.
+#' @export
+as.numeric.AntsResult = function(x, close = FALSE, ...) {
+    tour = x$best.tour
+    if (close)
+        tour = c(tour, tour[1])
+    return(tour)
+}
