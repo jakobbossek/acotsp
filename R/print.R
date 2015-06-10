cc2d9b8558a24488a8fd2cd671ad11592fd74088
Jakob Bossek <info@jakobbossek.de>
2015-06-10 11:58:28 +0200

    shorten and prettify output of print.AntsResult

--- R/print.R
+++ R/print.R
@@ -8,25 +8,7 @@
 #' @export
 print.AntsResult = function(x, ...) {
   n = getNumberOfNodes(x$network)
-  catf("ALGORITHM AND PARAMETERS")
-  catf("Result object of call:")
-  print(x$call)
-  catf("Parameters: %s", collapseList(x$used.arguments, sep = ", "))
-
-  catf("\nRESULT")
+  catf("Ants found solution after %i iterations (%.2f seconds passed)", x$iters.done, x$time.passed)
   catf("Tour length: %.5f", x$best.tour.length)
   catf("Tour: %s%s", collapse(x$best.tour[1:10], sep = ", "), if (n > 10) ", ..." else "")
-
-  catf("\nOPTIMIZATION PATH")
-  print(head(as.data.frame(x$opt.path)))
-  catf("...")
-}
-
-#FIXME: move this to BBmisc/HELL?
-collapseList = function(l, sep = ";") {
-  ns = names(l)
-  res = lapply(1:length(l), function(i) {
-    paste(ns[i], l[[i]], sep = "=")
-  })
-  collapse(unlist(res), sep)
 }
