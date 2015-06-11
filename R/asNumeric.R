a836ea03ee163d42e9c58f1a89d7148342ea2632
Jakob Bossek <info@jakobbossek.de>
2015-06-11 07:51:15 +0200

    cleanup

--- R/asNumeric.R
+++ R/asNumeric.R
@@ -3,13 +3,14 @@
 #' @param x [\code{AntResult}]\cr
 #'   Result object of a run of the \code{\link{aco}} function.
 #' @param close [\code{logical(1)}]\cr
-#'   Close the tour, i. e., append the last city to the end?
+#'   Close the tour, i. e., append the last city to the end? Default is \code{FALSE}.
 #' @param ... [any]\cr
 #'    Currenly not used.
 #' @export
 as.numeric.AntsResult = function(x, close = FALSE, ...) {
   tour = x$best.tour
-  if (close)
+  if (close) {
     tour = c(tour, tour[1])
+  }
   return(tour)
 }
